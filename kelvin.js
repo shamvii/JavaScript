efa40e58b6728ad837b8b2d3d394c9e9801fd891
shamvii <shambhavishukla24@gmail.com>
2016-09-22 19:07:36 -0400

    converting kelvin into Fahrenheit or Celsius

---
+++ kelvin.js
@@ -0,0 +1,5 @@
+//var kelvin = 293;
+var kelvin = prompt('What is the Kelvin temperature today?')
+var celsius = kelvin - 273;
+var fahrenheit = celsius * (9/5) + 32;
+console.log("Temperature in Fahrenheit today is around:" + fahrenheit);
