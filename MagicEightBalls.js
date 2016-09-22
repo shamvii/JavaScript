0a27142ba8b92d6fc63d2f528b1290a49ecea650
shamvii <shambhavishukla24@gmail.com>
2016-09-22 19:12:45 -0400

    Fortune-telling toy code

---
+++ MagicEightBalls.js
@@ -0,0 +1,33 @@
+var userQuestion = 'What is your question?';
+var randomNumber = Math.floor(Math.random() * 7);
+var eightBall = '';
+switch(randomNumber)
+  {
+    case 0:
+      eightBall = 'It is certain';
+      break;
+    case 1:
+      eightBall ='It is decidedly so ';
+      break;
+    case 2:
+      eightBall = 'Reply hazy try again';
+      break;
+    case 3: 
+      eightBall = 'Cannot predict now';
+      break;
+    case 4:
+     eightBall = 'Don\'t count on it';
+      break;
+    case 5:
+     eightBall = 'My sources say no';
+      break;
+    case 6:
+      eightBall = 'Outlook not so good';
+      break;
+    case 7:
+      eightBall = 'Signs point to yes';
+      break;
+      
+  }
+console.log('The user asked:' + userQuestion);
+console.log('The eight ball answered:' + eightBall);
