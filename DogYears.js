eefc2c9e12e013a5037ebe3fec0df19937019483
shamvii <shambhavishukla24@gmail.com>
2016-09-22 19:09:46 -0400

    calculating human age in Dog Years

---
+++ DogYears.js
@@ -0,0 +1,6 @@
+//var myAge = 27;
+var myAge = prompt('What is your age?');
+var firstYears = 2 * 10.5;
+var laterYears = (27 - 2) * 4;
+var myAgeInDogYears = firstYears + laterYears;
+console.log('My age in dog years is :' + myAgeInDogYears);
