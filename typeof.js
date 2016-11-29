d5b9c4afb34e551273fee24f46358dc6c7c4ffa4
shamvii <shambhavishukla24@gmail.com>
2016-11-29 18:32:31 -0500

    UsingtypeofinJS

---
+++ typeof.js
@@ -0,0 +1,8 @@
+//Using "typeof" 
+var anObj = { job: "I'm an object!" };
+var aNumber = 42;
+var aString = "I'm a string!";
+
+console.log( typeof anObj ); 
+console.log( typeof aNumber  ); 
+console.log( typeof aString ); 
