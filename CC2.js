7258d0973210a19cfed96fea9dbf34eb4909b8f7
Shamvi <shamvii@users.noreply.github.com>
2017-07-30 13:10:08 -0400

    Add files via upload
    Arrays and Objects in JavaScript

---
+++ CC2.js
@@ -0,0 +1,28 @@
+var years = [2001, 1985, 1994, 2014, 1973];
+
+function printFullAge() {
+var ages = [];
+var fullAges = [];
+for(var i = 0; i < years.length; i++){
+	ages[i] = 2016 - years[i];
+}
+
+for(i = 0; i < ages.length; i++){
+	if(ages[i] >= 18){
+		console.log('Person is ' + ages[i] + ' years old, and is of full age');
+		fullAges.push(true);
+	} else {
+		console.log('Person is ' + ages[i] + ' years old, and is not of full age');
+		fullAges.push(false);
+	}
+}
+return fullAges;
+}
+
+var years = [2001, 1985, 1994, 2014, 1973];
+var full_1 = printFullAge(years);
+var full_2 = printFullAge([2012, 1915, 1999]);
+
+
+
+
